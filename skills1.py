eac58d5e05b2d1e7e18f1b2a4f1924f4d981e506
Lavinia Karl <triciaroth@Tricia-Roths-MacBook.local>
2014-06-06 17:47:33 -0700

    all but last four solved

--- skills1.py
+++ skills1.py
@@ -26,29 +26,86 @@ print all_even([1,2,3,4,5,6,7,8])
 print all_even([1,3,5,7])
 print all_even([2,4,6,8])
 
-# Write a function that takes a list of strings and a new list with all strings of length 4 or greater.
+# Write a function that takes a list of strings and (creates?) a new list with all strings of length 4 or greater.
 def long_words(word_list):
-	return []
+	output=[]
+	for word in word_list:
+		if len(word)>=4:
+			output.append(word)
+	return output
+
+print "testing long_words:"
+print long_words(['hi','sup?','yo','yee','hyphy',"f'sho"])
 
 # Write a function that finds the smallest element in a list of integers and returns it.
 def smallest(some_list):
-	return None
+	# sorted_list=sorted(some_list)
+	# return sorted_list[0]
+	large=[]
+	for i in some_list:
+		for j in range(len(some_list)):
+			if i>some_list[j]:
+				large.append(i)
+	output=set(some_list)-set(large)
+	output=list(output)
+	return output[0]
+
+print "testing smallest:"
+print smallest([1,2,3,4,5])
+print smallest([7,6,5,4,3,2])
+print smallest([42,3,55,4,7])
 
 # Write a function that finds the largest element in a list of integers and returns it.
 def largest(some_list):
-	return None
+	small=[]
+	for i in some_list:
+		for j in range(len(some_list)):
+			if i<some_list[j]:
+				small.append(i)
+	output=set(some_list)-set(small)
+	output=list(output)
+	return output[0]
+
+print"testing largest:"
+print largest([1,2,3,4,5,6,7,8,9,10])
+print largest([2,4,3,1,6,5,9])
+print largest([8,5,6,4,3,7,2])
 
 # Write a function that takes a list of numbers and returns a new list of all those numbers divided by two.
 def halvesies(some_list):
-	return []
+	output=[]
+	for number in some_list:
+		half=number/2.0
+		output.append(half)
+	return output
+
+print "now testing halvesies:"
+print halvesies([-4,0,1,2,3,4,5,6,7,8,9,10])
 
 # Write a function that takes a list of words and returns a list of all the lengths of those words.
 def word_lengths(word_list):
-	return []
+	output=[]
+	for word in word_list:
+		length=len(word)
+		output.append(length)
+	return output
 
-# Write a function (using iteration) that sums all the numbers in a list.
+print "now testing word_lengths:"
+print word_lengths(['i','hi','bye'])
+
+Write a function (using iteration) that sums all the numbers in a list.
 def sum_numbers(numbers):
-	return []
+	i=0
+	for number in numbers:
+		i+=number
+	return i
+
+print "now testing sum_numbers:"
+print sum_numbers([1])
+print sum_numbers([1,4,-3])
+print sum_numbers([-8,10,1])
+
+
 
 # Write a function that multiplies all the numbers in a list together.
 def mult_numbers(numbers):
